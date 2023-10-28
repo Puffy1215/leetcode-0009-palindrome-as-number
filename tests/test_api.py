c423e38ab8b56c5ee02bba2d4c85e41a788340d4
Andy Xie <andy.xie.opensource@gmail.com>
2023-10-28 18:58:44 +0800

    test_api.py: Add test data

--- tests/test_api.py
+++ tests/test_api.py
@@ -8,8 +8,9 @@ from leetcode_0009_palindrome_number import api
 @pytest.mark.parametrize(
     ["result", "x"],
     (
-        [..., ...],
-        [..., ...],
+        [True, 121],
+        [False, -121],
+        [False, 10],
     ),
 )
 def test_palindrome_number(result: bool, x: int) -> None:
