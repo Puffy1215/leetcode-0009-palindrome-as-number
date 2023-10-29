3452bc37c3e51f3dbbf5ad6a4d47be9f4f3a8b9f
Andy Xie <andy.xie.opensource@gmail.com>
2023-10-29 21:02:30 +0800

    add arg

--- leetcode_0009_palindrome_number/api.py
+++ leetcode_0009_palindrome_number/api.py
@@ -3,13 +3,14 @@
 X_MAX = 2**31 - 1
 X_MIN = -(2**31)
 
+
 def _check_preconditions(x: int) -> bool:
     return X_MIN <= x <= X_MAX
 
 
-def palindrome_number(...) -> ...:
+def palindrome_number(x: int) -> bool:
     """Solves problem Palindrome Number"""
 
-    assert _check_preconditions(...)
+    assert _check_preconditions(x)
 
     pass
