db9ff04faad654172907bd42f029c144d3a1b892
Andy Xie <andy.xie.opensource@gmail.com>
2023-11-07 18:43:41 +0800

    implement _digits

--- leetcode_0009_palindrome_number/api.py
+++ leetcode_0009_palindrome_number/api.py
@@ -1,11 +1,17 @@
 """API for solving problem Palindrome Number"""
 
+import math
+
 X_MAX = 2**31 - 1
 X_MIN = -(2**31)
 
 
 def _check_preconditions(x: int) -> bool:
     return X_MIN <= x <= X_MAX
+
+
+def _digits(x: int) -> int:
+    return math.floor(math.log10(abs(x)) + 1) if x else 1
 
 
 def palindrome_number(x: int) -> bool:
